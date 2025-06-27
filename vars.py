1a69dbc097345b1da7ae9bc1b125f95d80d34f4f
binay23 <91482482+binay23@users.noreply.github.com>
2025-06-27 21:22:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,6 @@
+
 from os import environ
 
-API_ID = int(environ.get("API_ID", "20842776"))
-API_HASH = environ.get("API_HASH", "a5f3a00d014cccf4b1446e9e1a479270")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7207030782:AAFnY-k6EHLZqiNRyYvTerExHzYQuv-2-bY")
+API_ID = int(environ.get("API_ID", "23188718"))
+API_HASH = environ.get("API_HASH", "4f29b3178d1df6478009c188d790099a")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
